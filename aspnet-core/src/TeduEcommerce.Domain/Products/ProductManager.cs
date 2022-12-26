e1035bc8b881546064f1f30497ea594f7f27635f
VinhLTT-Eplus <thevinh.it.kh.1997@gmail.com>
2022-12-26 17:23:38 +0700

    5.21.1

---
+++ aspnet-core/src/TeduEcommerce.Domain/Products/ProductManager.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduEcommerce.ProductCategories;
+using Volo.Abp;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Services;
+
+namespace TeduEcommerce.Products
+{
+    public class ProductManager : DomainService
+    {
+        private readonly IRepository<Product, Guid> _productRepository;
+        private readonly IRepository<ProductCategory, Guid> _productCategoryRepository;
+        private readonly IMapper _mapper;
+        public ProductManager(IRepository<Product,Guid> productRepository,
+             IRepository<ProductCategory, Guid> productCategoryRepository, IMapper mapper)
+        {
+            _productCategoryRepository = productCategoryRepository;
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Product> CreateAsync(CreateUpdateProductDto input)
+        {
+            if (await _productRepository.AnyAsync(x => x.Name == input.Name))
+                throw new UserFriendlyException("Tên sản phẩm đã tồn tại", TeduEcommerceDomainErrorCodes.ProductNameAlreadyExists);
+            if (await _productRepository.AnyAsync(x => x.Code == input.Code))
+                throw new UserFriendlyException("Mã sản phẩm đã tồn tại", TeduEcommerceDomainErrorCodes.ProductCodeAlreadyExists);
+            if (await _productRepository.AnyAsync(x => x.SKU == input.SKU))
+                throw new UserFriendlyException("Mã SKU sản phẩm đã tồn tại", TeduEcommerceDomainErrorCodes.ProductSKUAlreadyExists);
+
+            var category =  await _productCategoryRepository.GetAsync(input.CategoryId);
+            var newProduct = new Product(Guid.NewGuid());
+            _mapper.Map(newProduct, newProduct);
+            newProduct.CategoryName = category.Name;
+            newProduct.CategorySlug = category.Slug;
+            return newProduct;
+        }
+    }
+}
