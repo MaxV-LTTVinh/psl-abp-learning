c1ec81838c979d55e0ffd452650256e3b3f0b26c
VinhLTT-Eplus <thevinh.it.kh.1997@gmail.com>
2022-12-29 14:25:11 +0700

    5.7

---
+++ aspnet-core/src/TeduEcommerce.Admin.Application.Contracts/ProductCategories/IProductCategoriesAppService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace TeduEcommerce.Admin.ProductCategories
+{
+    public interface IProductCategoriesAppService : ICrudAppService
+        <ProductCategoryDto,
+        Guid, 
+        PagedResultRequestDto,
+        CreateUpdateProductCategoryDto, 
+        CreateUpdateProductCategoryDto>
+    {
+        Task<PagedResultDto<ProductCategoryInListDto>> GetListFilterAsync(BaseListFilterDto input);
+    }
+}
